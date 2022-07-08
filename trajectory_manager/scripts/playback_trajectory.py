931f7f9af30d975d2e3ae4cf5e645b5ad4ffcdfc
jeroenzwan <j.zwanepol@hotmail.com>
2022-07-08 15:07:41 +0200

    added playback and record trajectory, it relies on importing from LfD

--- trajectory_manager/scripts/playback_trajectory.py
+++ trajectory_manager/scripts/playback_trajectory.py
@@ -1,3 +1,21 @@
 """
 Playback of trajectories and storing them into a databaseself.
 """
+from Learning_from_demonstration import LfD
+import sys
+
+if __name__ == '__main__':
+    lfd = LfD()
+    lfd.load(sys.argv[1])
+    
+    lfd.execute()
+
+    save = None
+    while not (save in [0,1]):
+        print("SAVE CURRENT RECORDING? 0 = NO, 1 = YES")
+        try:
+            save = int(input('\n'))
+        except:
+            print("INVALID INPUT")
+    if save:
+        lfd.save(sys.argv[1])
--- trajectory_manager/scripts/recording_trajectory.py
+++ trajectory_manager/scripts/recording_trajectory.py
@@ -1,3 +1,10 @@
 """
 Recording trajectories and storing them into a databaseself.
 """
+from Learning_from_demonstration import LfD
+import sys
+
+if __name__ == '__main__':
+    lfd = LfD()
+    lfd.traj_rec()
+    lfd.save(sys.argv[1])
