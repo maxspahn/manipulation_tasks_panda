0a8465e89060084a666d1d9321f8696f4ed51a83
nano2598 <nano2598.r@gmail.com>
2022-07-15 16:42:31 +0200

    changed Pose to PoseStamped since that is what we used throughout

--- manipulation_helpers/src/manipulation_helpers/pose_transform_functions.py
+++ manipulation_helpers/src/manipulation_helpers/pose_transform_functions.py
@@ -8,9 +8,9 @@ def orientation_2_quaternion(orientation):
 def position_2_array(position):
     return np.array([position.x, position.y, position.z])
 
-def pose_2_transformation(pose: Pose):
-    quaternion_orientation = orientation_2_quaternion(pose.orientation)
-    translation = position_2_array(pose.position)
+def pose_2_transformation(pose_st: PoseStamped):
+    quaternion_orientation = orientation_2_quaternion(pose_st.pose.orientation)
+    translation = position_2_array(pose_st.pose.position)
     rotation_matrix = quaternion.as_rotation_matrix(quaternion_orientation)
     transformation_matrix = np.identity(4)
     transformation_matrix[0:3, 0:3] = rotation_matrix
