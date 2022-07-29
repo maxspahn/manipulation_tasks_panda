149075d4ddbf890735df2e1321db96b2deb93282
jeroenzwan <j.zwanepol@hotmail.com>
2022-07-29 14:02:45 +0200

    changed class name in rec and pb traj

--- trajectory_manager/scripts/playback_trajectory.py
+++ trajectory_manager/scripts/playback_trajectory.py
@@ -1,7 +1,7 @@
 """
 Playback of trajectories and storing them into a databaseself.
 """
-from Learning_from_demonstration import LfD
+from Learning_from_Demonstration import Learning_from_Demonstration
 import sys
 import os
 
@@ -12,7 +12,7 @@ if __name__ == '__main__':
         print("Usage: " + os.path.basename(__file__) + " <trajectory_file_name>")
         sys.exit(1)
 
-    lfd = LfD()
+    lfd = Learning_from_Demonstration()
     lfd.load(arg1)
     
     lfd.execute()
